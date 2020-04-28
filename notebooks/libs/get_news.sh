3b3ecfd0de3df5aa548261f2ae56ad2164a92463
Abel Coronado <abxda@outlook.com>
2020-04-28 17:04:39 -0500

    Jupyter + Minio

---
+++ notebooks/libs/get_news.sh
@@ -0,0 +1,1 @@
+python /home/jovyan/work/libs/googler_new.py -n "$1" --news --json --tld mx --from "$5"/"$6"/"$4" --to "$5"/"$6"/"$4" "$2"  | tr -d '[\n]' | sed 's/},  {/}\n{/g' > /home/jovyan/work/"$7"/"$3"/"$4"-"$5"-"$6".json
